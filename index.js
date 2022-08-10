5322e9101707a9565efe52693781bf527f33af1c
ogola5 <ogolaevance5@gmail.com>
2022-08-10 12:20:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name: "ogola",
+    streetAddress: "sarchnar",
+}
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    let newem = {...employee}
+    newem[key] = value
+    return newem
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key] = value
+    return employee
+}
+function deleteFromEmployeeByKey(employee,key){
+    let nn = {...employee}
+    delete nn[key]
+    return nn
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key) {
+    delete employee[key]
+    return employee
+}
